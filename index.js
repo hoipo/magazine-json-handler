f88431fa0ba915b143d8f5367d1f5509ead4fae7
hoipo <578344975@qq.com>
2017-01-24 10:42:58 +0800

    add thumb image resize

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 const program = require('commander');
 const chalk = require('chalk');
 const path = require('path');
+const fse = require('fs-extra');
+const gm = require('gm');
+imageMagick = gm.subClass({ imageMagick: true });// install imageMagick.exe first, more info from http://www.imagemagick.org/script/binary-releases.php
 
 let data = {};
 
@@ -24,7 +27,7 @@ const handlerJson = (file) => {
     				delete element['nav-ver'];
     				delete element['nav-hor'];
     				delete element['pdf-hor'];
-    				let _ipadId = element['comment-url'].replace('http://magazine.pchouse.com.cn/','').split('/');
+    				let _ipadId = element['comment-url'].replace('http://magazine.pchouse.com.cn/','').replace('http://magazine.pcauto.com.cn/','').split('/');
     				element['ipad-article-id'] = _ipadId[1];
     				delete element['comment-url'];
     				_tmp.push(element)
@@ -38,11 +41,23 @@ const handlerJson = (file) => {
 	  if (err) throw err;
 	  console.log(chalk.green('It\'s done! ٩(ˊωˋ*)و'));
 	});
+
+    fse.walk(process.cwd())
+    .on('data', function(item) {
+        if(-1<path.basename(item.path).indexOf('thumb-ver.jpg')){
+            imageMagick(item.path).resize(500,500,"!").write(item.path, function(err){
+                if (err) return console.error(err)
+                 console.log(chalk.green(item.path + ' has been resize. ٩(ˊωˋ*)و'));
+            });
+        }
+
+    })
+
 }
 
 
 program
-    .version('0.0.1')
+    .version('0.0.2')
     .arguments('<file>')
     .action(function(file) {
         handlerJson(file)
